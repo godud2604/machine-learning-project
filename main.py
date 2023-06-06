665362cead69781644e5bacf5021b8ffd8dd721f
godud2604 <ees238@naver.com>
2023-06-06 23:15:01 +0900

    add, refactor: 훈련, 테스트 데이터 생성 & class funtion utils.py 파일로 분리

--- main.py
+++ main.py
@@ -1,60 +1,12 @@
 # %%
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as pgo
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_pinball_loss, mean_squared_error
 
 train = pd.read_csv('./data/train.csv')
 test = pd.read_csv('./data/test.csv')
 submission = pd.read_csv('./data/sampleSubmission.csv')
 
-print('train', train.shape)
-print('test', test.shape)
 # %%
-tr_tgt = train[['count']]
-
-# %%
-class Regressor:
-    
-    def __init__(self, tr_ftr, te_ftr, tr_tgt):
-        self.tr_ftr = tr_ftr
-        self.te_ftr = te_ftr
-        self.tr_tgt = tr_tgt
-
-    def _do_preprocessing(self):
-        scaler = StandardScaler()
-        self.tr_ftr = scaler.fit_transform(self.tr_ftr)
-        self.te_ftr = scaler.transform(self.te_ftr)
-
-        return self.tr_ftr, self.te_ftr
-
-    def do_visualize(self, target, predict):
-        fig = pgo.Figure(layout=dict(width=1000, height=600))
-        fig.add_trace(pgo.Scatter(x=np.arange(len(target)), y=target, name='true'))
-        fig.add_trace(pgo.Scatter(x=np.arange(len(target)), y=predict, name='pred'))        
-        fig.show()
-
-    def do_regression(self):
-        tr_ftr, te_ftr = self._do_preprocessing()
-        reg = RandomForestRegressor(n_jobs=-1)
-        reg.fit(tr_ftr, self.tr_tgt)
-        predict = reg.predict(te_ftr)
-
-        return predict
-
-    def do_calc_metric(self, target, predict):
-        mae = mean_absolute_error(target, predict)
-        mape = mean_absolute_percentage_error(target, predict)
-        mpl = mean_pinball_loss(target, predict)
-        mse = mean_squared_error(target, predict)
-        print(f'MAE : {mae}')
-        print(f'MAPE : {mape}')
-        print(f'PINBALL : {mpl}')
-        print(f'MSE : {mse}')
-
-# %%
-
-regressor = Regressor()
+tr_ftr = train.drop(['count'], axis=1)
+te_ftr = train.drop(['count'], axis=1)
+tr_tgt = train['count']
---
+++ utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as pgo
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_pinball_loss, mean_squared_error
+
+class Regressor:
+    def __init__(self, tr_ftr, te_ftr, tr_tgt):
+        self.tr_ftr = tr_ftr
+        self.te_ftr = te_ftr
+        self.tr_tgt = tr_tgt
+
+    def _do_preprocessing(self):
+        scaler = StandardScaler()
+        self.tr_ftr = scaler.fit_transform(self.tr_ftr)
+        self.te_ftr = scaler.transform(self.te_ftr)
+
+        return self.tr_ftr, self.te_ftr
+
+    def do_visualize(self, target, predict):
+        fig = pgo.Figure(layout=dict(width=1000, height=600))
+        fig.add_trace(pgo.Scatter(x=np.arange(len(target)), y=target, name='true'))
+        fig.add_trace(pgo.Scatter(x=np.arange(len(target)), y=predict, name='pred'))        
+        fig.show()
+
+    def do_regression(self):
+        tr_ftr, te_ftr = self._do_preprocessing()
+        reg = RandomForestRegressor(n_jobs=-1)
+        reg.fit(tr_ftr, self.tr_tgt)
+        predict = reg.predict(te_ftr)
+
+        return predict
+
+    def do_calc_metric(self, target, predict):
+        mae = mean_absolute_error(target, predict)
+        mape = mean_absolute_percentage_error(target, predict)
+        mpl = mean_pinball_loss(target, predict)
+        mse = mean_squared_error(target, predict)
+        print(f'MAE : {mae}')
+        print(f'MAPE : {mape}')
+        print(f'PINBALL : {mpl}')
+        print(f'MSE : {mse}')

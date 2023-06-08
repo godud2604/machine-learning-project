36f0fb848ac40f9f26d4eb68496cd905b23557b6
godud2604 <ees238@naver.com>
2023-06-08 22:48:48 +0900

    add: 데이터 시각화

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 datetime : 기록 일시 (1시간 간격)
 season : 계절 (1: 봄, 2: 여름, 3: 가을, 4: 겨울)
 holiday : 공휴일 여부 (0: 공휴일 아님, 1: 공휴일)
-workingday : 근무일 여부 (0: 근무일 아님, 1: 근무일)
+workingday : 근무일 여부 (0: 근무일 아님, 1: 근무일 - 공휴일과 주말을 뺀 나머지 날)
 weather : 날씨 (1: 맑음, 2: 옅은 안개, 3: 약간의 눈 또는 비와 천둥 번개, 4: 폭우와 천둥 번개 또는 눈과 짙은 안개)
 temp : 실제 온도
 atemp : 체감 온도
@@ -110,6 +110,140 @@ sns.displot(train['count']) # 분포도 출력
 """
 로그 변환
 => 데이터 분포를 정규분포에 가깝이 만들기 위해 사용하는 방법
+=> 후처리 : log(y)에서 실제 타깃값인 y를 복원하기 위해 지수변환을 해줘야 함
 """
 sns.displot(np.log(train['count']))
+
 # %%
+"""
+막대 그래프
+- 각 범주형 데이터에 따라 평균 대여 수량이 어떻게 다른지 파악 (피처 중요도 확인)
+- 아무 정보도 담고 있지 않은 피처는 모델 훈련에 사용하지 않는다
+
+* day, minute, second 피처 제거
+"""
+
+# 1. m행 n열 Figure 준비
+mpl.rc('font', size=14)
+mpl.rc('axes', titlesize=15) # 각 축의 제목 크기 설정
+figure, axes = plt.subplots(nrows=3, ncols=2) # 3행 2열 Figure 생성
+plt.tight_layout() # 그래프 사이에 여백 확보
+figure.set_size_inches(10, 9) # 전체 Figure 크기를 10x9인치로 설정
+
+# 2. 각 축에 서브플롯 할당
+sns.barplot(x='year', y='count', data=train, ax=axes[0, 0])
+sns.barplot(x='month', y='count', data=train, ax=axes[0, 1])
+sns.barplot(x='day', y='count', data=train, ax=axes[1, 0])
+sns.barplot(x='hour', y='count', data=train, ax=axes[1, 1])
+sns.barplot(x='minute', y='count', data=train, ax=axes[2, 0])
+sns.barplot(x='second', y='count', data=train, ax=axes[2, 1])
+
+# 3. 세부 설정
+axes[0, 0].set(title="Rental amounts by year")
+axes[0, 1].set(title="Rental amounts by month")
+axes[1, 0].set(title="Rental amounts by day")
+axes[1, 1].set(title="Rental amounts by hour")
+axes[2, 0].set(title="Rental amounts by minute")
+axes[2, 1].set(title="Rental amounts by second")
+
+# 1행에 위치한 서브플롯들의 x축 라벨 90도 회전
+axes[1, 0].tick_params(axis='x', labelrotation=90)
+axes[1, 1].tick_params(axis='x', labelrotation=90)
+# %%
+"""
+박스플롯
+- 범주형 데이터에 따른 수치형 데이터 정보를 나타내는 그래프
+- 막대 그래프보다 더 많은 정보를 제공하는 특징
+
+* 공휴일이 아닐 때와 근무일일 때 이상치(outlier)가 많다.
+"""
+
+# 1. m행 n열 Figure 준비
+figure, axes = plt.subplots(nrows=2, ncols=2)
+plt.tight_layout()
+figure.set_size_inches(10, 10)
+
+# 2. 서브플롯 할당
+sns.boxplot(x='season', y='count', data=train, ax=axes[0, 0])
+sns.boxplot(x='weather', y='count', data=train, ax=axes[0, 1])
+sns.boxplot(x='holiday', y='count', data=train, ax=axes[1, 0])
+sns.boxplot(x='workingday', y='count', data=train, ax=axes[1, 1])
+
+# 3. 세부 설정
+# 서브플롯에 제목 달기
+axes[0, 0].set(title="Box Plot On Count Across Season")
+axes[0, 1].set(title="Box Plot On Count Across Weather")
+axes[1, 0].set(title="Box Plot On Count Across Holiday")
+axes[1, 1].set(title="Box Plot On Count Across Working Day")
+
+# x축 라벨 겹침 해결
+axes[0, 1].tick_params(axis='x', labelrotation=10)
+# %%
+
+"""
+포인트플롯
+- 범주형 데이터에 따른 수치형 데이터의 평균과 신뢰구간을 점과 선으로 표시
+- 막대 그래프와 동일한 정보를 제공하지만, 한 화면에 여러 그래프를 그려 서로 비교해보기에 더 적합하다.
+
+* weather == 4인 데이터 제거 (이상치)
+"""
+
+# 1. m행 n열 Figure 준비
+mpl.rc('font', size=11)
+figure, axes = plt.subplots(nrows=5) # 5행 1열
+figure.set_size_inches(12, 18)
+
+# 2. 서브플롯 할당
+# 근무일, 공휴일, 요일, 계절, 날씨에 따른 시간대별 평균 대여 수량 포인트플롯
+# hue 파라미터에 비교하고 싶은 피처 전달
+sns.pointplot(x='hour', y='count', data=train, hue='workingday', ax=axes[0])
+sns.pointplot(x='hour', y='count', data=train, hue='holiday', ax=axes[1])
+sns.pointplot(x='hour', y='count', data=train, hue='weekday', ax=axes[2])
+sns.pointplot(x='hour', y='count', data=train, hue='season', ax=axes[3])
+sns.pointplot(x='hour', y='count', data=train, hue='weather', ax=axes[4])
+# %%
+"""
+회귀선을 포함한 산점도 그래프
+- 데이터 간 상관관계를 파악하는 데 사용
+- 회귀선 기울기로 대략적인 추세를 파악할 수 있다.
+
+* windspeed 피처 제거
+- 결측치가 많다. (풍속이 0인 데이터 다수 존재)
+"""
+
+# 1. m행 n열 Figure 준비
+mpl.rc('font', size=15)
+figure, axes = plt.subplots(nrows=2, ncols=2)
+plt.tight_layout()
+figure.set_size_inches(7, 6)
+
+# 2. 서브플롯 할당
+# 온도, 체감 온도, 풍속, 습도 별 대여 수량 산점도 그래프
+sns.regplot(x='temp', y='count', data=train, ax=axes[0, 0],
+            scatter_kws={'alpha': 0.2}, line_kws={'color': 'blue'})
+sns.regplot(x='atemp', y='count', data=train, ax=axes[0, 1],
+            scatter_kws={'alpha': 0.2}, line_kws={'color': 'blue'})
+sns.regplot(x='windspeed', y='count', data=train, ax=axes[1, 0],
+            scatter_kws={'alpha': 0.2}, line_kws={'color': 'blue'})
+sns.regplot(x='humidity', y='count', data=train, ax=axes[1, 1],
+            scatter_kws={'alpha': 0.2}, line_kws={'color': 'blue'})
+
+
+
+# %%
+"""
+히트맵
+- 수치형 데이터끼리 어떤 상관관계가 있는지 확인 가능
+- 피처들의 수많은 조합 사이의 관계를 한눈에 파악 가능.
+
+* windspeed와 count의 상관관계는 0.1
+=> 상관관계가 매우 약하다. 즉, 수량 예측에 큰 도움이 안 됨
+=> windspeed 피처 제거
+"""
+
+# 피처 간 상관관계 매트릭스
+corrMat = train[['temp', 'atemp', 'humidity', 'windspeed', 'count']].corr()
+fig, ax = plt.subplots()
+fig.set_size_inches(10, 10)
+sns.heatmap(corrMat, annot=True) 
+ax.set(title="Heatmap of Numerical Data")
